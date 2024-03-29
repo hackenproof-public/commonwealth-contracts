9d833116c50057c8724d1cc226f5acf771c23098
dszymaniak <95844353+dszymaniak@users.noreply.github.com>
2024-03-29 07:52:20 +0100

    Genesis NFT Vesting bonus setup (#99)
    * script
    
    * PR fixes

---
+++ deploy/605_genesis_nft_bonus_setup.ts
@@ -0,0 +1,34 @@
+import { ethers } from 'hardhat';
+import { DeployFunction } from 'hardhat-deploy/dist/types';
+import { HardhatRuntimeEnvironment } from 'hardhat/types';
+import { getZkSyncSingerWallet } from '../utils/zkSyncWallet';
+import { confirmYesOrNo } from '../scripts/utils';
+
+const genesisNftBonusSetup: DeployFunction = async (hre: HardhatRuntimeEnvironment) => {
+  console.log('Setup started');
+
+  const genesisNftVestingAddress = '0x'; // needs to be redeployed
+  const bonusSetupArray: number[] = []; // [1,2,3,6,87] as hex values: [0x1,0x2,0x3,0x6,0x57]
+
+  if (!genesisNftVestingAddress) {
+    throw Error(
+      'Please configure genesisNftVestingAddress'
+    );
+  }
+
+  const wallet = getZkSyncSingerWallet();
+
+  const vesting = (await ethers.getContractAt('GenesisNFTVesting', genesisNftVestingAddress, wallet));
+
+  console.log("loaded bonus setup array: "+bonusSetupArray);
+
+
+  if (await confirmYesOrNo('Do you want to deploy contract? [y/N] ')) {
+    console.log(`Setting bonuses... `);
+    const tx = await vesting.connect(wallet).setupBonus(bonusSetupArray);
+    await tx.wait(1);
+  }
+};
+
+export default genesisNftBonusSetup;
+genesisNftBonusSetup.tags = ['genesisNftBonusSetup'];
