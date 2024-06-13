c37d12a2e488d491d20dee2b459001dd61bbc7fc
barisCW <baris@common-wealth.io>
2024-06-13 10:17:04 +0300

    Dynamic Genesis NFTs

--- contracts/interfaces/IGenesisNFT.sol
+++ contracts/interfaces/IGenesisNFT.sol
@@ -1,7 +1,18 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.18;
 
+import {IGenesisNFTVesting} from "./IGenesisNFTVesting.sol";
+
 interface IGenesisNFT {
+    /**
+     * @notice Metadata structure for NFT
+     */
+    struct Metadata {
+        string name;
+        string description;
+        string image;
+        string externalUrl;
+    }
     /**
      * @notice Emitted when zkSync GenesisNFTmirror notification about moving a token is sent
      * @param tokenId id of a transferred token
@@ -18,8 +29,83 @@ interface IGenesisNFT {
     event OwnershipTransferred(address indexed previousOwner, address indexed newOwner);
 
     /**
+     * @notice Emitted when metadata for NFT is changed
+     * @param name Metadata name
+     * @param description Metadata description
+     * @param image Metadata image
+     * @param externalUrl Metadata external URL
+     */
+    event MetadataChanged(string indexed name, string indexed description, string indexed image, string externalUrl);
+
+    /**
+     * @notice Emitted when metadata name for NFT is changed
+     * @param name Metadata name
+     */
+    event MetadataNameChanged(string indexed name);
+
+    /**
+     * @notice Emitted when metadata description for NFT is changed
+     * @param description Metadata description
+     */
+    event MetadataDescriptionChanged(string indexed description);
+
+    /**
+     * @notice Emitted when metadata image for NFT is changed
+     * @param image Metadata image
+     */
+    event MetadataImageChanged(string indexed image);
+
+    /**
+     * @notice Emitted when metadata external URL for NFT is changed
+     * @param externalUrl Metadata external URL
+     */
+    event MetadataExternalUrlChanged(string indexed externalUrl);
+
+    /**
+     * @notice Set new metadata name
+     * @param _name New metadata name
+     */
+    function setMetadataName(string memory _name) external;
+
+    /**
+     * @notice Set new metadata description
+     * @param _description New metadata description
+     */
+    function setMetadataDescription(string memory _description) external;
+
+    /**
+     * @notice Set new metadata image
+     * @param _image New metadata image
+     */
+    function setMetadataImage(string memory _image) external;
+
+    /**
+     * @notice Set new metadata external URL
+     * @param _extenralUrl New metadata external URL
+     */
+    function setMetadataExternalUrl(string memory _extenralUrl) external;
+
+    /**
+     * @notice Set all metadata at once
+     * @param _metadata Metadata structure
+     */
+    function setAllMetadata(Metadata memory _metadata) external;
+
+    /**
      * @notice Returns Genesis NFT series number
      * @return Genesis NFT series number
      */
     function getSeries() external view returns (uint256);
+
+    /**
+     * @notice Returns Unvested Tokens
+     * @return Unvested Tokens
+     */
+    function fetchTokenDetails(uint256 _tokenId) external view returns (uint256);
+
+    /**
+     * @notice Returns Number of Slices
+     * @return Number of slices
+     */
+    function getSlices(uint256 _tokenId) external view returns (uint256);
 }
