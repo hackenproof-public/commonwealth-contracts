ebd1c471b0c73c1dc6903e10ddd572acaf0fc641
dszymaniak <szymaniakdominik@gmail.com>
2024-08-01 08:31:58 +0200

    alphafund upgrade

--- test/unit/InvestmentNFT.test.ts
+++ test/unit/InvestmentNFT.test.ts
@@ -241,6 +241,38 @@ describe('Investment NFT unit tests', () => {
           investmentNft.connect(user).split(tokenId, [minimumValue.sub(1), toUsdc('71')])
         ).to.be.revertedWithCustomError(investmentNft, 'InvestmentNft__InvestmentTooLow');
       });
+
+      it('should revert splitting nft when listed on marketplace', async function () {
+        const { investmentNft, user, minter, owner, deployer } = await loadFixture(deployFixture);
+        const newName = 'New Name';
+        const newDescription = 'New Description';
+        const newImage = 'New Image';
+        const newUrl = 'New Url';
+        const newMetadata = {
+          name: newName,
+          description: newDescription,
+          image: newImage,
+          externalUrl: newUrl
+        };
+        const tokenValue = toUsdc('50');
+        const [communityFund, genesisNftRoyaltyAccount] = await ethers.getSigners();
+        const wlth: FakeContract<Wlth> = await smock.fake('Wlth');
+        const marketplace = (await deployProxy(
+          'Marketplace',
+          [owner.address, wlth.address, communityFund.address, genesisNftRoyaltyAccount.address],
+          deployer
+        )) as Marketplace;
+  
+        await investmentNft.connect(minter).mint(user.address, tokenValue);
+        await investmentNft.connect(owner).setMarketplaceAddress(marketplace.address);
+        await marketplace.connect(owner).addAllowedContract(investmentNft.address);
+
+        await investmentNft.connect(user).approve(marketplace.address, 0);
+        await marketplace.connect(user).listNFT(investmentNft.address, 0, toWlth('500'), true);
+
+        await expect(investmentNft.connect(user).split(tokenId, [toUsdc('50'), toUsdc('70')]))
+        .to.be.revertedWithCustomError(investmentNft, 'InvestmentNft__TokenListed');
+      });
     });
   });
 
